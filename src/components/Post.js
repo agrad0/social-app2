9ce48cd01b50a341a017ee4f328728ffb2366d75
agrad0 <110928774+agrad0@users.noreply.github.com>
2023-01-15 16:33:02 +0100

    14.01 ev

--- src/components/Post.js
+++ src/components/Post.js
@@ -10,7 +10,9 @@ const Post = (props) => {
                 <img src={props.postData.user.avatar_url} alt={props.postData.user.username} />
             </div>  
             <div className="post-content">
-                  
+                <h5>Data dodania: {props.postData.created_at}</h5>
+                
+                <article>{props.postData.content}</article>
 
             </div>
         </div>
